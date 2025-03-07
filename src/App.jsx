f4e3d83a761799872c8311517f93c67d07b8511e
Lea Simone <apple@MacBookPro.attlocal.net>
2025-03-06 18:54:08 -0600

    final  update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import {useState, useEffect} from "react";
-import "./App.css";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+import Form from './components/Form'
+import MovieDisplay from './components/MovieDisplay'
 
-import MovieDisplay from "./components/MovieDisplay";
-import Form from "./components/Form";
-
-export default function App() {
+function App() {
   // Constant with your API Key
   const apiKey = "c633ca89";
 
   // State to hold movie data
   const [movie, setMovie] = useState(null);
 
-  // Function to get movies
-  const getMovie = async(searchTerm) => {
-    // Make fetch request and store the response
-    const response = await fetch(
-      `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
-    );
-    // Parse JSON response into a JavaScript object
-    const data = await response.json();
-    // Set the Movie state to the received data
-    setMovie(data);
-  };
+  const movieSearch = async (searchTerm) => {
+    try {
+      const res = await axios(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`);
+      setMovie(res.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+// useEffect(() => {
+  //   const searchById = async(retries = 5) => {
+  //     if (retries === 0) {
+  //       console.error("Failed to find a valid movie after multiple attempts.")
+  //       return;
+  //     }
+
+  //     const randomNumber = Math.floor(Math.random()*100000000)
+  //     const movieId = randomNumber.toString().padStart(7, '0')
+  
+  //     try {
+  //       const res = await axios(`http://www.omdbapi.com/?apikey=${apiKey}&i=tt${movieId}`);
+  //       if(res.data.Error){
+  //         console.warn(`Movie not found for ID tt${movieId}. Retrying...`);
+  //         await searchById(retries - 1);
+  //       } else {
+  //         setMovie(res.data)
+  //         return;
+  //       }
+  //     } catch (e) {
+  //       console.error(e)
+  //     }
+  //   }
+
+  //   searchById(10);
+  // }, [])
+
+
 
   // We pass the getMovie function as a prop called moviesearch
+  const defaultMovies = ['Clueless', 'Knives Out', 'Wicked', 'Paddington', 'The Holy Mountain', 'Shock Treatment' ]
+
+  useEffect(()=>{
+    const randomIdx = Math.floor(Math.random()* defaultMovies.length)
+    movieSearch(defaultMovies[randomIdx]);
+  }, [])
+
   return (
-    <div className="App">
-      <Form movieSearch={getMovie} />
-      <MovieDisplay movie={movie}/> 
-    </div>
-  );
+    <>
+      <Form movieSearch={movieSearch}/>
+      <MovieDisplay movie={movie} />
+    </>
+  )
 }
+
+export default App
--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,20 +1,28 @@
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 export default function MovieDisplay({movie}) {
-  const loaded = () =>{ 
-  return (
-      <div>
-          <h1>{movie.Title}</h1>
-          <h2>{movie.Genre}</h2>
-          <img src={movie.Poster} alt={movie.Title} />
-          <h2>{movie.Year}</h2>
+    const loaded = () =>{ 
+    return (
+        <div>
+            <h1>{movie.Title}</h1>
+            <h2>{movie.Genre}</h2>
+            <img src={movie.Poster} alt={movie.Title} />
+            <h2>{movie.Year}</h2>
 
-      </div>
-  )}
+        </div>
+    )}
 
-  const loading = () => {
-      return (
-          <h1>Movie not found</h1>
-      )
-  }
+    const loading = () => {
+        return (
+            <div>
+                <h1><Skeleton highlightColor='#f5f5f5'/></h1>
+                <h2><Skeleton /></h2>
+                <Skeleton height="450px"/>
+                <h2><Skeleton /></h2>
+            </div>
+        )
+    }
 
-  return movie ? loaded() : loading();
+    return movie ? loaded() : loading();
 }
